457b8cca4a3d994412371260e3e55b783873348d
avinash84319 <avihanvaisri@gmail.com>
2025-03-14 11:38:47 +0530

    changes: poetry and faker

--- scripts/fake_data_generation.py
+++ scripts/fake_data_generation.py
@@ -7,6 +7,11 @@ import string
 import os
 import pandas as pd
 from tabulate import tabulate
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
 # Database Credentials (Use environment variables)
 DB_NAME = os.getenv("DB_NAME", "ERPDB")
 USER = os.getenv("DB_USER", "root")
